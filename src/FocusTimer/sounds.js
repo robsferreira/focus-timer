abb732db09717feb87cbe4f63a7db90b5685e45f
Robson <52553115+robsferreira@users.noreply.github.com>
2024-07-20 02:33:32 -0300

    Add files via upload

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,11 @@
+export const buttonPressAudio = new Audio('./assets/sounds/button-press.wav')
+
+export const kitchenTimer = new Audio('./assets/sounds/kichen-timer.mp3')
+
+export const bgAudio = new Audio('./assets/sounds/bg-audio.mp3')
+
+export const WomanInChains = new Audio('./assets/sounds/WomanInChains.mp3')
+
+bgAudio.loop = true
+
+WomanInChains.loop = true
